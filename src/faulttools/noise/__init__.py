379e0d2350a77a0d4c381d78b8d1eb60cb587290
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-12-14 14:22:51 +0000

    Move noise model into its own package

---
+++ src/faulttools/noise/__init__.py
@@ -0,0 +1,2 @@
+from .model import NoiseModel as NoiseModel, Fault as Fault
+from .conversion import to_noise_model as to_noise_model
--- src/faulttools/pushout.py
+++ src/faulttools/pushout.py
@@ -1,6 +1,6 @@
 from faulttools import PauliString
 from faulttools.flip_operators import FlipOperators
-from faulttools.noise_model import NoiseModel, Fault
+from faulttools.noise import NoiseModel, Fault
 
 
 def pushout(model: NoiseModel, flip_ops: FlipOperators) -> NoiseModel:
