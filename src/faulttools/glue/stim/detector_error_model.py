d594e4ed6e5e3a02eef052e11f8804624b471e0a
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2025-12-28 15:50:33 +0100

    Fix after changes in flip operators

--- src/faulttools/glue/stim/detector_error_model.py
+++ src/faulttools/glue/stim/detector_error_model.py
@@ -13,7 +13,6 @@ def _flip_ops_for_detecting_operators(
     measurement_edges = {flip_ops.diagram.incident_edges(n)[0]: i for i, n in enumerate(measurement_nodes)}
 
     region_gen_set: list[PauliString] = []
-    region_flip_ops: list[PauliString] = []
     for operator in operators:
         compiled_gen_set = GF2(
             [p.restrict(measurement_edges.keys()).compile(measurement_edges) for p in flip_ops.region_gen_set]
@@ -26,29 +25,19 @@ def _flip_ops_for_detecting_operators(
                 b[measurement_edges[e] + len(operator)] = 1
         rref = GF2(np.vstack([compiled_gen_set, [b]])).transpose().row_reduce()
         solution = PauliString({})
-        solution_flip_op = PauliString({})
         for row in rref:
             nonzero_indices = np.nonzero(row)[0]
             if len(nonzero_indices) > 0 and row[-1] == 1:
                 if nonzero_indices[0] == len(compiled_gen_set):  # Got a full 0 row except for augmenting column
                     raise RuntimeError(f"Could not solve detecting regions for operator {operator}!")
                 solution *= flip_ops.region_gen_set[nonzero_indices[0]]
-                solution_flip_op *= flip_ops.region_flip_ops[nonzero_indices[0]]
         region_gen_set.append(solution)
-        region_flip_ops.append(solution_flip_op)
-
-    region_flip_op_stab_flip_map = {
-        i: {j for j in range(len(flip_ops.stab_gen_set)) if not region_flip_ops[i].commutes(flip_ops.stab_gen_set[j])}
-        for i in range(len(region_flip_ops))
-    }
 
     return FlipOperators(
         flip_ops.diagram,
         flip_ops.stab_flip_ops.copy(),
-        region_flip_ops,
         flip_ops.stab_gen_set.copy(),
         region_gen_set,
-        region_flip_op_stab_flip_map,
     )
 
 
