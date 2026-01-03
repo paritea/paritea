45049c879798c17694cb0693bde81ce6c2a39a64
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2026-01-03 03:29:20 +0100

    Speed up unfolding by using custom priority queue

--- src/faulttools/equivalence/enumeration.py
+++ src/faulttools/equivalence/enumeration.py
@@ -1,4 +1,3 @@
-import heapq
 import itertools
 import time
 
@@ -159,118 +158,124 @@ def _regular_strategy(
             print("No signatures to match for g2!")
         return None
 
-    g1_sink_mask = (1 << g1_sinks) - 1
-    g2_sink_mask = (1 << g2_sinks) - 1
-
     g1_atomic_lookup: dict[int, int] = {}
     for sig, v in g1_sigs:
         if sig in g1_atomic_lookup and g1_atomic_lookup[sig] <= v:
             continue
         g1_atomic_lookup[sig] = v
-    g1_pq = [(v, sig) for sig, v in g1_atomic_lookup.items()]
-    heapq.heapify(g1_pq)
+
+    g1_pq: dict[int, list[int]] = {}
+    for sig, v in g1_atomic_lookup.items():
+        if v not in g1_pq:
+            g1_pq[v] = []
+        g1_pq[v].append(sig)
 
     g2_atomic_lookup: dict[int, int] = {}
     for sig, v in g2_sigs:
         if sig in g2_atomic_lookup and g2_atomic_lookup[sig] <= v:
             continue
         g2_atomic_lookup[sig] = v
-    g2_pq = [(v, sig) for sig, v in g2_atomic_lookup.items()]
-    heapq.heapify(g2_pq)
+    g2_pq: dict[int, list[int]] = {}
+    for sig, v in g2_atomic_lookup.items():
+        if v not in g2_pq:
+            g2_pq[v] = []
+        g2_pq[v].append(sig)
 
-    g1_stats: dict[int, int] = {}
-    for v, _ in g1_pq:
-        if v not in g1_stats:
-            g1_stats[v] = 0
-        g1_stats[v] += 1
-    g2_stats: dict[int, int] = {}
-    for v, _ in g2_pq:
-        if v not in g2_stats:
-            g2_stats[v] = 0
-        g2_stats[v] += 1
+    g1_sink_mask = (1 << g1_sinks) - 1
+    g2_sink_mask = (1 << g2_sinks) - 1
 
-    max_weight = 0
+    w = 0
     t = tqdm(desc="Weight", initial=0, leave=False, disable=quiet, unit="", bar_format="{desc}: {n_fmt}", ncols=0)
     while len(g2_pq) > 0:
-        max_weight += 1
+        w += 1
         t.update()
 
+        queue = g1_pq.pop(w, [])
         g1_stats_t = tqdm(
             desc="G1 sigs remaining: ",
-            initial=g1_stats.get(max_weight, 0),
-            total=g1_stats.get(max_weight, 0),
+            initial=len(queue),
+            total=len(queue),
             leave=False,
             disable=quiet,
             position=1,
             unit="",
         )
-        while len(g1_pq) > 0 and g1_pq[0][0] <= max_weight:
-            w, sig = heapq.heappop(g1_pq)
-            g1_stats[w] -= 1
-            g1_stats_t.update(n=-1)
-            if sig & g1_sink_mask > 0:
-                if sig in g1_detectable_lookup and g1_detectable_lookup[sig] <= w:
-                    continue  # This signature does not provide a weight improvement
-                g1_detectable_lookup[sig] = w
-            else:
-                sig_no_sinks = sig >> g1_sinks
-                if sig_no_sinks in g1_undetectable_lookup and g1_undetectable_lookup[sig_no_sinks] <= w:
-                    continue  # This signature does not provide a weight improvement
-                g1_undetectable_lookup[sig_no_sinks] = w
+        while len(queue) > 0:
+            new_queue = []
+            for sig in queue:
+                g1_stats_t.update(n=-1)
+                if sig & g1_sink_mask > 0:
+                    if sig in g1_detectable_lookup and g1_detectable_lookup[sig] <= w:
+                        continue  # This signature does not provide a weight improvement
+                    g1_detectable_lookup[sig] = w
+                else:
+                    sig_no_sinks = sig >> g1_sinks
+                    if sig_no_sinks in g1_undetectable_lookup and g1_undetectable_lookup[sig_no_sinks] <= w:
+                        continue  # This signature does not provide a weight improvement
+                    g1_undetectable_lookup[sig_no_sinks] = w
 
-            if sig in g1_atomic_lookup and g1_atomic_lookup[sig] > w:
-                g1_atomic_lookup[sig] = w
+                if sig in g1_atomic_lookup and g1_atomic_lookup[sig] > w:
+                    g1_atomic_lookup[sig] = w
 
-            for atomic_sig, atomic_w in g1_atomic_lookup.items():
-                comb_w = atomic_w + w
-                heapq.heappush(g1_pq, (comb_w, atomic_sig ^ sig))
-                g1_stats[comb_w] = g1_stats.get(comb_w, 0) + 1
-                if comb_w == max_weight:
-                    g1_stats_t.update(n=1)
+                for atomic_sig, atomic_w in g1_atomic_lookup.items():
+                    comb_w = atomic_w + w
+                    if comb_w == w:
+                        new_queue.append(atomic_sig ^ sig)
+                        g1_stats_t.update(n=1)
+                    else:
+                        if comb_w not in g1_pq:
+                            g1_pq[comb_w] = []
+                        g1_pq[comb_w].append(atomic_sig ^ sig)
+            queue = new_queue
         g1_stats_t.close()
-        tqdm.write(f"Finished unfolding weight {max_weight} in queue 1! Items remaining: {len(g1_pq)}...")
+        tqdm.write(f"Finished unfolding weight {w} in queue 1! Next items remaining: {len(g1_pq.get(w + 1, []))}...")
 
+        queue = g2_pq.pop(w, [])
         g2_stats_t = tqdm(
             desc="G2 sigs remaining: ",
-            initial=g2_stats.get(max_weight, 0),
-            total=g2_stats.get(max_weight, 0),
+            initial=len(queue),
+            total=len(queue),
             leave=False,
             disable=quiet,
             position=1,
             unit="",
         )
-        while len(g2_pq) > 0 and g2_pq[0][0] <= max_weight:
-            w, sig = heapq.heappop(g2_pq)
-            g2_stats[w] -= 1
-            g2_stats_t.update(n=-1)
-            if sig in g2_lookup and g2_lookup[sig] <= w:
-                continue  # This signature does not provide a weight improvement
-            g2_lookup[sig] = w
+        while len(queue) > 0:
+            new_queue = []
+            for sig in queue:
+                g2_stats_t.update(n=-1)
+                if sig in g2_lookup and g2_lookup[sig] <= w:
+                    continue  # This signature does not provide a weight improvement
+                g2_lookup[sig] = w
 
-            if sig & g2_sink_mask == 0:
-                sig_no_sinks = sig >> g2_sinks
-                if sig_no_sinks not in g1_undetectable_lookup:
-                    if not quiet:
-                        tqdm.write(
-                            f"{_format_sig(sig, g2_boundaries, g2_sinks)} has no equivalent in g1, or it was not "
-                            f"yet generated and thus has higher weight!"
-                        )
-                    return max_weight
+                if sig & g2_sink_mask == 0:
+                    sig_no_sinks = sig >> g2_sinks
+                    if sig_no_sinks not in g1_undetectable_lookup:
+                        if not quiet:
+                            tqdm.write(
+                                f"{_format_sig(sig, g2_boundaries, g2_sinks)} has no equivalent in g1, or it was not "
+                                f"yet generated and thus has higher weight!"
+                            )
+                        return w
 
-            if sig in g2_atomic_lookup and g2_atomic_lookup[sig] > w:
-                g2_atomic_lookup[sig] = w
+                if sig in g2_atomic_lookup and g2_atomic_lookup[sig] > w:
+                    g2_atomic_lookup[sig] = w
 
-            for atomic_sig, atomic_w in g2_atomic_lookup.items():
-                comb_w = atomic_w + w
-                heapq.heappush(g2_pq, (comb_w, atomic_sig ^ sig))
-                g2_stats[comb_w] = g2_stats.get(comb_w, 0) + 1
-                if comb_w == max_weight:
-                    g2_stats_t.update(n=1)
+                for atomic_sig, atomic_w in g2_atomic_lookup.items():
+                    comb_w = atomic_w + w
+                    if comb_w == w:
+                        new_queue.append(atomic_sig ^ sig)
+                        g2_stats_t.update(n=1)
+                    else:
+                        if comb_w not in g2_pq:
+                            g2_pq[comb_w] = []
+                        g2_pq[comb_w].append(atomic_sig ^ sig)
+            queue = new_queue
         g2_stats_t.close()
-        tqdm.write(f"Finished unfolding weight {max_weight} in queue 2! Items remaining: {len(g2_pq)}...")
+        tqdm.write(f"Finished unfolding weight {w} in queue 2! Next items remaining: {len(g2_pq.get(w + 1, []))}...")
     t.close()
 
     if len(g1_pq) == 0 and len(g2_pq) != 0:
-        return max_weight  # We ran out of signatures to generate for g1
+        return w  # We ran out of signatures to generate for g1
 
     return None
