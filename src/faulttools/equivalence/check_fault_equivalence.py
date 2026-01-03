6ed4fc28b406c2ba2fcab5abfc55b3b6de274dbc
Maximilian Rüsch <xammaximus.ruesch@gmail.com>
2026-01-03 01:56:13 +0100

    Add strategy for non-normally weighted noise models

--- src/faulttools/equivalence/check_fault_equivalence.py
+++ src/faulttools/equivalence/check_fault_equivalence.py
@@ -4,11 +4,11 @@ import numpy as np
 from galois import GF2
 
 from faulttools import build_flip_operators, push_out
-from faulttools.noise import NoiseModel
+from faulttools.noise import Fault, NoiseModel
 from faulttools.pauli import Pauli, PauliString
 from faulttools.utils import NoiseModelParam, noise_model_params
 
-from .enumeration import _smallest_size_iteration
+from .enumeration import _normal_strategy, _regular_strategy
 
 
 class Stabilisers:
@@ -55,17 +55,18 @@ def _compile_atomic_faults(
     stabilisers: AugmentedStabilisers,
     boundaries_to_idx: Mapping[int, int],
     detector_to_idx: Mapping[int, int],
-) -> list[GF2]:
-    normalised_faults: list[GF2] = []
-    for f in noise.atomic_faults():
+) -> list[tuple[int, int]]:
+    normalised_faults: list[tuple[int, int]] = []
+    for f, vs in noise.atomic_faults_with_values():
         if f.is_trivial():
             continue
 
         compiled = f.compile(boundaries_to_idx, detector_to_idx)
-        normalised_faults.append(stabilisers.normalise_single(compiled))
-    sig_nf = [GF2(l) for l in np.unique(normalised_faults, axis=0)]
+        normalised = stabilisers.normalise_single(compiled)
+        normalised_int = Fault.compiled_to_int(normalised)
+        normalised_faults.extend((normalised_int, v) for v in vs)
 
-    return sig_nf
+    return normalised_faults
 
 
 def _is_fault_equivalence(
@@ -93,11 +94,10 @@ def _is_fault_equivalence(
     """
     atomic_weights_1 = {w for _, w in noise_1.atomic_faults_with_values_unpacked()}
     atomic_weights_2 = {w for _, w in noise_2.atomic_faults_with_values_unpacked()}
-    if atomic_weights_1 != {1} or atomic_weights_2 != {1}:
-        raise ValueError(
-            f"Both given noise models must be equally and normally weighted. "
-            f"Weight sets detected: {atomic_weights_1} and {atomic_weights_2}"
-        )
+    use_normally_weighted_strategy = atomic_weights_1 == {1} and atomic_weights_2 == {1} and False
+    if not quiet:
+        print(f"Using {'normal' if use_normally_weighted_strategy else 'regular'} strategy!")
+    strategy = _normal_strategy if use_normally_weighted_strategy else _regular_strategy
 
     d1, d2 = noise_1.diagram, noise_2.diagram
     d1_edge_idx_map = {d1.incident_edges(b)[0]: i for i, b in enumerate(d1.io_sorted())}
@@ -112,28 +112,24 @@ def _is_fault_equivalence(
     g1_stabs = AugmentedStabilisers.from_stabilisers(compiled_stabilisers, len(d1_detector_idx_map))
     g1_sig_nf = _compile_atomic_faults(noise_1, g1_stabs, d1_edge_idx_map, d1_detector_idx_map)
     if not quiet:
-        print(f"Retrieved {len(g1_sig_nf)} unique faults for d1!")
+        print(f"Retrieved {len(g1_sig_nf)} atomic faults for d1!")
 
     if not quiet:
         print("Compiling atomic faults for d2...")
     g2_stabs = AugmentedStabilisers.from_stabilisers(compiled_stabilisers, len(d2_detector_idx_map))
     g2_sig_nf = _compile_atomic_faults(noise_2, g2_stabs, d2_edge_idx_map, d2_detector_idx_map)
     if not quiet:
-        print(f"Retrieved {len(g2_sig_nf)} unique faults for d2!")
+        print(f"Retrieved {len(g2_sig_nf)} atomic faults for d2!")
 
     if not quiet:
         print("Checking if d1 is fault-bound by d2...")
-    g1_g2_weight = _smallest_size_iteration(
-        g1_sig_nf, g2_sig_nf, num_detectors_1, len(d2_edge_idx_map), num_detectors_2, quiet=quiet
-    )
+    g1_g2_weight = strategy(g1_sig_nf, g2_sig_nf, num_detectors_1, len(d2_edge_idx_map), num_detectors_2, quiet=quiet)
     if g1_g2_weight is not None:
         return False
 
     if not quiet:
         print("Checking if d2 is fault-bound by d1...")
-    g2_g1_weight = _smallest_size_iteration(
-        g2_sig_nf, g1_sig_nf, num_detectors_2, len(d1_edge_idx_map), num_detectors_1, quiet=quiet
-    )
+    g2_g1_weight = strategy(g2_sig_nf, g1_sig_nf, num_detectors_2, len(d1_edge_idx_map), num_detectors_1, quiet=quiet)
     return g2_g1_weight is None
 
 
--- src/faulttools/equivalence/enumeration.py
+++ src/faulttools/equivalence/enumeration.py
@@ -1,10 +1,8 @@
+import heapq
 import itertools
 import time
 
-from galois import GF2
 from tqdm.auto import tqdm
-
-from faulttools.noise import Fault
 
 
 def _format_sig(sig: int, boundaries: int, sinks: int) -> str:
@@ -16,9 +14,9 @@ def _format_sig(sig: int, boundaries: int, sinks: int) -> str:
     return f"[{b_str}  ||  {' '.join(sig_str[boundaries * 2 :])}]"
 
 
-def _smallest_size_iteration(
-    g1_sig_nf: list[GF2],
-    g2_sig_nf: list[GF2],
+def _normal_strategy(
+    g1_sigs: list[tuple[int, int]],
+    g2_sigs: list[tuple[int, int]],
     g1_sinks: int,
     g2_boundaries: int,
     g2_sinks: int,
@@ -40,26 +38,27 @@ def _smallest_size_iteration(
     g1_lookup = {0: 0}  # The trivial signature requires zero signatures to generate
     g2_lookup = {}
 
-    if len(g2_sig_nf) == 0:
+    g1_unique_sigs = {f for f, w in g1_sigs}
+    g2_unique_sigs = {f for f, w in g2_sigs}
+
+    if len(g2_unique_sigs) == 0:
         if not quiet:
             print("No signatures to match for g2!")
         return None
-    num_max_signatures = 2 ** ((len(g2_sig_nf[0]) - g2_sinks) // 2 + g2_sinks)
+    num_max_signatures = 2 ** (g2_boundaries // 2 + g2_sinks)
     num_total_signatures = 0
 
     if not quiet:
-        print(f"Starting iteration until {len(g2_sig_nf)}!")
+        print(f"Starting iteration until {len(g2_unique_sigs)}!")
     g1_last_new_signatures = [0]
     g2_last_new_detectable = [0]
-    g1_sig_nf_ints = [Fault.compiled_to_int(sig) for sig in g1_sig_nf]
-    g2_sig_nf_ints = [Fault.compiled_to_int(sig) for sig in g2_sig_nf]
     g1_sink_mask = (1 << g1_sinks) - 1
     g2_sink_mask = (1 << g2_sinks) - 1
     for max_size in tqdm(
-        range(1, len(g2_sig_nf_ints) + 1),
+        range(1, len(g2_unique_sigs) + 1),
         desc="Weight: ",
         initial=1,
-        total=len(g2_sig_nf_ints),
+        total=len(g2_unique_sigs),
         leave=False,
         disable=quiet,
     ):
@@ -68,7 +67,7 @@ def _smallest_size_iteration(
         # Populate g1_lookup for this weight
         g1_time = time.time()
         g1_new_signatures = []
-        for last_it_int, atomic_sig_nf_int in itertools.product(g1_last_new_signatures, g1_sig_nf_ints):
+        for last_it_int, atomic_sig_nf_int in itertools.product(g1_last_new_signatures, g1_unique_sigs):
             combined_sig = last_it_int ^ atomic_sig_nf_int
             if combined_sig & g1_sink_mask > 0:
                 continue  # Detectable g1 signatures will never be queried, save space here
@@ -86,9 +85,9 @@ def _smallest_size_iteration(
         num_new_signatures = 0
         g2_new_detectable = []
         for last_it_int, atomic_sig_nf_int in tqdm(
-            itertools.product(g2_last_new_detectable, g2_sig_nf_ints),
+            itertools.product(g2_last_new_detectable, g2_unique_sigs),
             desc="Signatures: ",
-            total=len(g2_last_new_detectable) * len(g2_sig_nf_ints),
+            total=len(g2_last_new_detectable) * len(g2_unique_sigs),
             leave=False,
             disable=quiet,
         ):
@@ -125,5 +124,109 @@ def _smallest_size_iteration(
                     f"Discovered {num_new_signatures} new signatures this iteration (so far: {num_total_signatures}, "
                     f"max signatures: {num_max_signatures})!"
                 )
+
+    return None
+
+
+def _regular_strategy(
+    g1_sigs: list[tuple[int, int]],
+    g2_sigs: list[tuple[int, int]],
+    g1_sinks: int,
+    g2_boundaries: int,
+    g2_sinks: int,
+    *,
+    quiet: bool = True,
+) -> int | None:
+    """
+    Takes fault signatures of g1,g2 in normal form (stabilisers factored out) where the sink containment information is
+    provided in the last `..._sinks` elements of the signature.
+
+    Determines the smallest size of a combination `comb_sig` from elements of `g2_sig_nf` such that
+
+    - `comb_sig` does not enable any sinks and is thus not detectable AND EITHER
+    - `comb_sig` does not have an equivalent in `g1_sig_nf` (without sink information) OR
+    - the equivalent of `comb_sig` in `g1_sig_nf` has a greater size
+
+    :returns: the size of such a combination or `None` if no such combination exists.
+    """
+    # The trivial signature requires zero signatures to generate
+    g1_detectable_lookup = {}
+    g1_undetectable_lookup = {0: 0}
+    g2_lookup = {0: 0}
+
+    if len(g2_sigs) == 0:
+        if not quiet:
+            print("No signatures to match for g2!")
+        return None
+
+    g1_sink_mask = (1 << g1_sinks) - 1
+    g2_sink_mask = (1 << g2_sinks) - 1
+
+    g1_atomic_lookup: dict[int, int] = {}
+    for sig, v in g1_sigs:
+        if sig in g1_atomic_lookup and g1_atomic_lookup[sig] <= v:
+            continue
+        g1_atomic_lookup[sig] = v
+    g1_pq = [(v, sig) for sig, v in g1_atomic_lookup.items()]
+    heapq.heapify(g1_pq)
+
+    g2_atomic_lookup: dict[int, int] = {}
+    for sig, v in g2_sigs:
+        if sig in g2_atomic_lookup and g2_atomic_lookup[sig] <= v:
+            continue
+        g2_atomic_lookup[sig] = v
+    g2_pq = [(v, sig) for sig, v in g2_atomic_lookup.items()]
+    heapq.heapify(g2_pq)
+
+    max_weight = 0
+    t = tqdm(desc="Weight: ", initial=0, leave=False, disable=quiet)
+    while len(g2_pq) > 0:
+        max_weight += 1
+        t.update()
+
+        while len(g1_pq) > 0 and g1_pq[0][0] <= max_weight:
+            w, sig = heapq.heappop(g1_pq)
+            if sig & g1_sink_mask > 0:
+                if sig in g1_detectable_lookup and g1_detectable_lookup[sig] <= w:
+                    continue  # This signature does not provide a weight improvement
+                g1_detectable_lookup[sig] = w
+            else:
+                sig_no_sinks = sig >> g1_sinks
+                if sig_no_sinks in g1_undetectable_lookup and g1_undetectable_lookup[sig_no_sinks] <= w:
+                    continue  # This signature does not provide a weight improvement
+                g1_undetectable_lookup[sig_no_sinks] = w
+
+            if sig in g1_atomic_lookup and g1_atomic_lookup[sig] > w:
+                g1_atomic_lookup[sig] = w
+
+            for atomic_sig, atomic_w in g1_atomic_lookup.items():
+                heapq.heappush(g1_pq, (atomic_w + w, atomic_sig ^ sig))
+        tqdm.write(f"Finished unfolding weight {max_weight} in queue 1! Items remaining: {len(g1_pq)}...")
+
+        while len(g2_pq) > 0 and g2_pq[0][0] <= max_weight:
+            w, sig = heapq.heappop(g2_pq)
+            if sig in g2_lookup and g2_lookup[sig] <= w:
+                continue  # This signature does not provide a weight improvement
+            g2_lookup[sig] = w
+
+            if sig & g2_sink_mask == 0:
+                sig_no_sinks = sig >> g2_sinks
+                if sig_no_sinks not in g1_undetectable_lookup:
+                    if not quiet:
+                        tqdm.write(
+                            f"{_format_sig(sig, g2_boundaries, g2_sinks)} has no equivalent in g1, or it was not "
+                            f"yet generated and thus has higher weight!"
+                        )
+                    return max_weight
+
+            if sig in g2_atomic_lookup and g2_atomic_lookup[sig] > w:
+                g2_atomic_lookup[sig] = w
+
+            for atomic_sig, atomic_w in g2_atomic_lookup.items():
+                heapq.heappush(g2_pq, (atomic_w + w, atomic_sig ^ sig))
+        tqdm.write(f"Finished unfolding weight {max_weight} in queue 2! Items remaining: {len(g2_pq)}...")
+
+    if len(g1_pq) == 0 and len(g2_pq) != 0:
+        return max_weight  # We ran out of signatures to generate for g1
 
     return None
