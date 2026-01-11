195747c0ab70943bc07f3ade52515ab2eeb4b15e
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2026-01-11 15:45:44 +0100

    fix: Correct generation of shor circuits (#16)

--- src/paritea/generate/diagram/syndrome/shor.py
+++ src/paritea/generate/diagram/syndrome/shor.py
@@ -1,5 +1,7 @@
 from typing import Literal, overload
 
+from mpl_toolkits.axes_grid1.axes_size import Fraction
+
 from paritea import Pauli, PauliString
 from paritea.diagram import Diagram, NodeType
 
@@ -53,47 +55,59 @@ def shor_extraction(
         new_nodes = []
         for stabiliser in stabilisers:
             stabiliser_diagram = Diagram()
-            mapped = [-1 for _ in range(qubits)]
+            first = [-1 for _ in range(qubits)]
+            last = [-1 for _ in range(qubits)]
             controls = [-1 for _ in range(qubits)]
 
-            cat_x = stabiliser_diagram.add_node(NodeType.X, x=row_offset, y=qubits + 2 + qubits / 2)
+            cat_z = stabiliser_diagram.add_node(NodeType.Z, x=row_offset, y=qubits + 2 + qubits / 2)
             row_offset += 1
 
             # Generate Pauli boxes
             for idx, pauli in stabiliser.items():
                 if pauli == Pauli.I:
                     continue
-                elif pauli == Pauli.Y:
-                    raise ValueError("Measuring Pauli Y is not supported!")
 
-                target_node = stabiliser_diagram.add_node(NodeType.Z, x=row_offset, y=idx)
-                c = stabiliser_diagram.add_node(NodeType.X, x=row_offset, y=idx + qubits + 2)
+                target_node = stabiliser_diagram.add_node(NodeType.X, x=row_offset, y=idx)
+                c = stabiliser_diagram.add_node(NodeType.Z, x=row_offset, y=idx + qubits + 1)
+                stabiliser_diagram.add_edge(target_node, c)
+
                 if pauli == Pauli.X:
-                    # X-Pauli is provided with retracted H-box to facilitate single nodes in array
-                    h = stabiliser_diagram.add_node(NodeType.H, x=row_offset, y=qubits + 1)
-                    stabiliser_diagram.add_edge(target_node, h)
-                    stabiliser_diagram.add_edge(h, c)
+                    h1 = stabiliser_diagram.add_node(NodeType.H, x=row_offset - 0.5, y=idx)
+                    h2 = stabiliser_diagram.add_node(NodeType.H, x=row_offset + 0.5, y=idx)
+                    stabiliser_diagram.add_edge(target_node, h1)
+                    stabiliser_diagram.add_edge(target_node, h2)
+                    first[idx] = h1
+                    last[idx] = h2
+                elif pauli == Pauli.Y:
+                    x1 = stabiliser_diagram.add_node(NodeType.X, phase=Fraction(1, 2), x=row_offset - 0.5, y=idx)
+                    x2 = stabiliser_diagram.add_node(NodeType.X, phase=Fraction(-1, 2), x=row_offset + 0.5, y=idx)
+                    stabiliser_diagram.add_edge(target_node, x1)
+                    stabiliser_diagram.add_edge(target_node, x2)
+                    first[idx] = x1
+                    last[idx] = x2
                 else:
-                    stabiliser_diagram.add_edge(target_node, c)
+                    first[idx] = target_node
+                    last[idx] = target_node
 
-                mapped[idx] = target_node
                 controls[idx] = c
                 row_offset += 1
 
             # Connect to cat state
             for i, c in enumerate(controls):
-                measure = stabiliser_diagram.add_node(NodeType.X, x=row_offset, y=qubits + i + 2)
+                h = stabiliser_diagram.add_node(NodeType.H, x=row_offset, y=qubits + i + 1)
+                measure = stabiliser_diagram.add_node(NodeType.X, x=row_offset + 1, y=qubits + i + 1)
+                stabiliser_diagram.add_edge(h, measure)
                 if c == -1:
-                    stabiliser_diagram.add_edge(cat_x, measure)
+                    stabiliser_diagram.add_edge(cat_z, h)
                 else:
-                    stabiliser_diagram.add_edge(cat_x, c)
-                    stabiliser_diagram.add_edge(c, measure)
+                    stabiliser_diagram.add_edge(cat_z, c)
+                    stabiliser_diagram.add_edge(c, h)
 
-            row_offset += 1
+            row_offset += 2
 
             # Append to overall diagram
-            trans = d.compose(stabiliser_diagram, {b: n for b, n in zip(current_qubit_nodes, mapped) if n != -1})
-            current_qubit_nodes = [trans[n] if n != -1 else current_qubit_nodes[i] for i, n in enumerate(mapped)]
+            trans = d.compose(stabiliser_diagram, {b: n for b, n in zip(current_qubit_nodes, first) if n != -1})
+            current_qubit_nodes = [trans[n] if n != -1 else current_qubit_nodes[i] for i, n in enumerate(last)]
             if granular:
                 node_list.append(list(trans.values()))
             else:
