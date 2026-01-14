90df678d749ee2c196378dc450cb0cabdefdc20b
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2026-01-14 15:37:40 +0000

    perf: Reduce number of atomic faults unfolded during fault equivalence enumeration (#21)

--- src/paritea/equivalence/enumeration.py
+++ src/paritea/equivalence/enumeration.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 import time
 from collections.abc import Iterator
 from dataclasses import dataclass, field
@@ -15,132 +16,49 @@ def _format_sig(sig: int, boundaries: int, sinks: int) -> str:
     return f"[{b_str}  ||  {' '.join(sig_str[boundaries * 2 :])}]"
 
 
-def _normal_strategy(
-    g1_sigs: list[tuple[int, int]],
-    g2_sigs: list[tuple[int, int]],
-    g1_sinks: int,
-    g2_boundaries: int,
-    g2_sinks: int,
-    *,
-    until: int | None = None,
-    quiet: bool = True,
-) -> int | None:
-    """
-    Takes fault signatures of g1,g2 in normal form (stabilisers factored out) where the sink containment information is
-    provided in the last `..._sinks` elements of the signature.
-
-    Determines the smallest size of a combination `comb_sig` from elements of `g2_sig_nf` such that
-
-    - `comb_sig` does not enable any sinks and is thus not detectable AND EITHER
-    - `comb_sig` does not have an equivalent in `g1_sig_nf` (without sink information) OR
-    - the equivalent of `comb_sig` in `g1_sig_nf` has a greater size
-
-    :returns: the size of such a combination or `None` if no such combination exists.
-    """
-    g1_undetectable_lookup = {0: 0}  # The trivial signature requires zero signatures to generate
-    g1_detectable_lookup = {}
-    g2_lookup = {}
-
-    g1_unique_sigs = {f for f, w in g1_sigs}
-    g2_unique_sigs = {f for f, w in g2_sigs}
-
-    if len(g2_unique_sigs) == 0:
-        if not quiet:
-            print("No signatures to match for g2!")
-        return None
-    num_max_signatures = 2 ** (g2_boundaries + g2_sinks)
-    num_total_signatures = 0
-
-    if not quiet:
-        print(f"Starting iteration until {len(g2_unique_sigs)}!")
-    g1_last_new_signatures = [0]
-    g2_last_new_detectable = [0]
-    g1_sink_mask = (1 << g1_sinks) - 1
-    g2_sink_mask = (1 << g2_sinks) - 1
-    max_weight = min(len(g2_unique_sigs), until - 1) if until is not None else len(g2_unique_sigs)
-    for max_size in tqdm(
-        range(1, max_weight + 1), desc="Weight: ", initial=1, total=max_weight, leave=False, disable=quiet
-    ):
-        if not quiet:
-            tqdm.write(f"Starting iteration with combinatory size: {max_size}...")
-        # Populate g1_lookup for this weight
-        g1_time = time.time()
-        g1_new_signatures = []
-        for last_it_int, atomic_sig_nf_int in itertools.product(g1_last_new_signatures, g1_unique_sigs):
-            combined_sig = last_it_int ^ atomic_sig_nf_int
-            if combined_sig & g1_sink_mask > 0:
-                if combined_sig not in g1_detectable_lookup:
-                    g1_new_signatures.append(combined_sig)
-                    g1_detectable_lookup[combined_sig] = max_size
-            else:
-                combined_sig_no_sinks = combined_sig >> g1_sinks
-                if combined_sig_no_sinks not in g1_undetectable_lookup:
-                    g1_new_signatures.append(combined_sig)
-                    g1_undetectable_lookup[combined_sig_no_sinks] = max_size
-        g1_last_new_signatures = g1_new_signatures
-        if not quiet:
-            tqdm.write(f"Populating g1 lookup took {time.time() - g1_time}s.")
-
-        # Incrementally discover g2 signatures by combining #`max_size` atomic signatures
-        g2_time = time.time()
-        num_new_signatures = 0
-        g2_new_detectable = []
-        for last_it_int, atomic_sig_nf_int in tqdm(
-            itertools.product(g2_last_new_detectable, g2_unique_sigs),
-            desc="Signatures: ",
-            total=len(g2_last_new_detectable) * len(g2_unique_sigs),
-            leave=False,
-            disable=quiet,
-        ):
-            combined_sig = last_it_int ^ atomic_sig_nf_int
-            if combined_sig in g2_lookup:
-                continue  # Already discovered
-
-            num_new_signatures += 1
-            g2_lookup[combined_sig] = max_size
-            if combined_sig & g2_sink_mask > 0:
-                g2_new_detectable.append(combined_sig)
-                continue  # Detectable
-
-            # Perform search with real output signature
-            if combined_sig >> g2_sinks not in g1_undetectable_lookup:
-                if not quiet:
-                    tqdm.write(
-                        f"{_format_sig(combined_sig, g2_boundaries, g2_sinks)} has no equivalent in g1, or it was not "
-                        f"yet generated and thus has higher weight!"
-                    )
-                return max_size  # No equivalent error with equal or lower weight found
-        if not quiet:
-            tqdm.write(f"Populating g2 lookup took {time.time() - g2_time}s.")
-
-        g2_last_new_detectable = g2_new_detectable
-        num_total_signatures += num_new_signatures
-        if num_new_signatures == 0:
-            if not quiet:
-                tqdm.write("No new signatures discovered!")
-            break
-        else:
-            if not quiet:
-                tqdm.write(
-                    f"Discovered {num_new_signatures} new signatures this iteration (so far: {num_total_signatures}, "
-                    f"max signatures: {num_max_signatures})!"
-                )
-
-    return None
-
-
 @dataclass(init=True)
 class AtomicFaults:
     weight_lookup: dict[int, int] = field(default_factory=dict, init=False)
+    undetectable: set[int] = field(default_factory=set, init=False)
+    detectable_with_detectors: dict[int, int] = field(default_factory=dict, init=False)
 
     def all_iter(self) -> Iterator[tuple[int, int]]:
-        return iter(self.weight_lookup.items())
+        for sig in itertools.chain(self.undetectable, self.detectable_with_detectors.keys()):
+            yield sig, self.weight_lookup[sig]
+
+    def detector_overlapping(self, detector_info: int) -> list[int]:
+        lowest_weight = math.inf
+        lowest_weight_sigs = []
+        for sig, sig_info in self.detectable_with_detectors.items():
+            if detector_info & sig_info == 0:
+                continue
+            w = self.weight_lookup[sig]
+            if w > lowest_weight:
+                continue
+            if w == lowest_weight:
+                lowest_weight_sigs.append(sig)
+            else:
+                lowest_weight = w
+                lowest_weight_sigs = [sig]
+
+        return lowest_weight_sigs
 
 
-def prepare_atomic_faults(nm_sigs: list[tuple[int, int]]) -> AtomicFaults:
+def prepare_atomic_faults(nm_sigs: list[tuple[int, int]], *, num_detectors: int) -> AtomicFaults:
     atomics: AtomicFaults = AtomicFaults()
+    detector_mask = (1 << num_detectors) - 1
     for sig, v in nm_sigs:
-        if sig in atomics.weight_lookup and atomics.weight_lookup[sig] <= v:
+        detectable = sig & detector_mask > 0
+        if detectable:
+            if sig not in atomics.detectable_with_detectors:
+                atomics.detectable_with_detectors[sig] = sig & detector_mask
+                atomics.weight_lookup[sig] = v
+        else:
+            if sig not in atomics.undetectable:
+                atomics.undetectable.add(sig)
+                atomics.weight_lookup[sig] = v
+
+        if atomics.weight_lookup[sig] <= v:
             continue
         atomics.weight_lookup[sig] = v
 
@@ -188,7 +106,9 @@ def _next_gen_unfold(
         items_done += len(queue)
         for sig in queue:
             sigs_pgb.update(n=-1)
-            if sig & detector_mask > 0:
+            detector_info = sig & detector_mask
+            detectable = detector_info > 0
+            if detectable:
                 if sig in detectable_lookup and detectable_lookup[sig] <= w:
                     continue  # This signature does not provide a weight improvement
                 detectable_lookup[sig] = w
@@ -202,8 +122,9 @@ def _next_gen_unfold(
             if sig in atomics.weight_lookup and atomics.weight_lookup[sig] > w:
                 atomics.weight_lookup[sig] = w
 
-            for atomic_sig, atomic_w in atomics.all_iter():
-                comb_w = atomic_w + w
+            atomic_faults = atomics.undetectable if not detectable else atomics.detector_overlapping(detector_info)
+            for atomic_sig in atomic_faults:
+                comb_w = atomics.weight_lookup[atomic_sig] + w
                 if comb_w == w:
                     new_queue.add(atomic_sig ^ sig)
                     sigs_pgb.update(n=1)
@@ -253,10 +174,10 @@ def _next_gen_strategy(
     nm2_detectable_lookup = {}
     nm2_undetectable_lookup = {0: 0}
 
-    nm1_atomics = prepare_atomic_faults(nm1_sigs)
+    nm1_atomics = prepare_atomic_faults(nm1_sigs, num_detectors=d1_detectors)
     nm1_pq = prepare_priority_queue(nm1_atomics)
 
-    nm2_atomics = prepare_atomic_faults(nm2_sigs)
+    nm2_atomics = prepare_atomic_faults(nm2_sigs, num_detectors=d2_detectors)
     nm2_pq = prepare_priority_queue(nm2_atomics)
 
     w = 0
