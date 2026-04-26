bea8bb593f1533f7ba05669b90af9628fccfd8fb
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-04-26 17:14:17 +0200

    Remove surface code

--- src/paritea/generate/diagram/__init__.py
+++ src/paritea/generate/diagram/__init__.py
@@ -1,9 +1,7 @@
 from .clifford import clifford
-from .surface_code import surface_code_memory_experiment
 from .zweb import zweb
 
 __all__ = [
     "clifford",
-    "surface_code_memory_experiment",
     "zweb",
 ]
