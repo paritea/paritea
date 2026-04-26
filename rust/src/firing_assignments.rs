223202e7df6de1df82e31277fc432abea4aba876
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-04-27 00:49:33 +0200

    Way faster firing verification

--- rust/src/firing_assignments.rs
+++ rust/src/firing_assignments.rs
@@ -97,13 +97,9 @@ pub fn create_firing_verification(d: &Diagram, ordering: &GraphOrdering) -> BitM
     }
     // Subtract identity for pi/2 spiders in the bottom right corner
     let num_pi_2 = ordering.pi_2_spiders.len();
-    for i_row in 0..num_pi_2 {
-        for i_col in 0..num_pi_2 {
-            if i_row == i_col {
-                let (r, c) = (rows - num_pi_2 + i_row, cols - num_pi_2 + i_col);
-                m_d.set_bit(r, c, m_d.bit(r, c) ^ true);
-            }
-        }
+    for i in 0..num_pi_2 {
+        let (r, c) = (rows - num_pi_2 + i, cols - num_pi_2 + i);
+        m_d.set_bit(r, c, m_d.bit(r, c) ^ true);
     }
 
     m_d
