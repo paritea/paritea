4d7e5dd54ee790386f6cfcf2168cb15bb58c958e
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-05-04 11:11:03 +0200

    Use new circuit builder

--- src/paritea/generate/diagram/surface_code.py
+++ src/paritea/generate/diagram/surface_code.py
@@ -5,7 +5,7 @@
 from itertools import chain
 from typing import Literal, NamedTuple, overload
 
-from paritea.diagram import Diagram, NodeType
+from paritea.diagram import Diagram
 from paritea.generate.circuit_builder import CircuitBuilder
 
 
@@ -43,6 +43,7 @@ def surface_code_memory_experiment(
 
     :param distance: The distance (side length) of the surface code.
     :param rounds: The number of times to repeat the diagram.
+    :param partition: Whether to return partitions that aid during Pauli web computation.
     :return:
     """
     if rounds < 1:
@@ -152,19 +153,19 @@ def _generate_rotated_surface_code(
     partitions = []
     # Start building the circuit
     builder = CircuitBuilder(max(all_qubits) + 1)
-    inputs = builder.append_single_qubit(data_qubits, n_type=NodeType.B)
+    inputs = builder.append_boundary(data_qubits)
     for _ in range(rounds):
         new_nodes = []
 
-        new_nodes.extend(builder.append_reset(measurement_qubits, n_type=NodeType.X))
-        new_nodes.extend(builder.append_single_qubit(x_measurement_qubits, n_type=NodeType.H))
+        new_nodes.extend(builder.append_reset_zero(measurement_qubits))
+        new_nodes.extend(builder.append_h(x_measurement_qubits))
         for cts in cnot_cts:
             new_nodes.extend(builder.append_cnot(cts))
-        new_nodes.extend(builder.append_single_qubit(x_measurement_qubits, n_type=NodeType.H))
-        new_nodes.extend(builder.append_measure(measurement_qubits, n_type=NodeType.X))
+        new_nodes.extend(builder.append_h(x_measurement_qubits))
+        new_nodes.extend(builder.append_measure_comp(measurement_qubits))
 
         partitions.append(new_nodes)
-    outputs = builder.append_single_qubit(data_qubits, n_type=NodeType.B)
+    outputs = builder.append_boundary(data_qubits)
     builder.diagram.set_io(inputs, outputs, virtual=False)
 
     return builder.diagram, partitions
