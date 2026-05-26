7d1faa551c61509352626be970fadf5ec1046280
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-05-26 23:41:18 +0100

    Revert "Dial down distances"
    This reverts commit e760b81da5b4acf26b4b83c45dc97e7a084c9f82.

--- benchmarks/bench_web_compute.py
+++ benchmarks/bench_web_compute.py
@@ -28,7 +28,7 @@ from paritea.web.compute import (
 )
 from paritea.web.partitions import pauli_webs_through_partitions
 
-DISTANCES = [3, 5, 7]
+DISTANCES = [3, 5, 7, 9]
 ROUNDS = [1, 2, 3, 5, 10, 15, 20]
 
 
