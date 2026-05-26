72c47f51035303606f3d055b3fc4f4ea06e27032
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2026-05-27 00:10:56 +0100

    feat: Enable checking diagram semantics and check so during fault equivalence (#30)

--- src/paritea/web/__init__.py
+++ src/paritea/web/__init__.py
@@ -1,7 +1,24 @@
+import numpy as np
+from galois import GF2
+
+from paritea.pauli import PauliString
+
 from .compute import compute_detecting_regions, compute_pauli_webs, compute_stabilisers
+
+
+def row_reduce_webs(webs: list[PauliString], idx_map: dict[int, int]) -> GF2:
+    """Produces a row-reduction of the provided webs, only including the keys of the
+    index map, at the index indicated by the keys entry in the map."""
+    np_stabilisers = np.zeros((len(webs), len(idx_map) * 2), dtype=int)
+    for i, stab in enumerate(webs):
+        np_stabilisers[i, :] = stab.restrict(idx_map.keys()).compile(idx_map)
+
+    return GF2(np_stabilisers).row_reduce(eye="left")
+
 
 __all__ = [
     "compute_detecting_regions",
     "compute_pauli_webs",
     "compute_stabilisers",
+    "row_reduce_webs",
 ]
---
+++ tests/equivalence/test_semantic_equivalence.py
@@ -0,0 +1,27 @@
+from fractions import Fraction
+
+from paritea.diagram import Diagram, NodeType
+from paritea.equivalence.semantic import is_semantic_equivalence
+from paritea.generate import surface_code_memory_experiment
+
+
+def test_removing_pi_2_phase():
+    # Replacing two pi/2 phase spiders with one is a change in semantics
+    d1 = Diagram()
+    z1, z2 = d1.add_node(NodeType.Z, phase=Fraction(1, 2)), d1.add_node(NodeType.Z, phase=Fraction(1, 2))
+    d1.add_edges([(d1.add_node(NodeType.B), z1), (z1, z2), (z2, d1.add_node(NodeType.B))])
+
+    d2 = Diagram()
+    z = d2.add_node(NodeType.Z, phase=Fraction(1, 2))
+    d2.add_edges([(d2.add_node(NodeType.B), z), (z, d2.add_node(NodeType.B))])
+
+    assert not is_semantic_equivalence(d1, d2)
+
+
+def test_surface_code_multi_round():
+    """Tests that one round of a surface code memory experiment implements the same+
+    linear map as two rounds."""
+    d1 = surface_code_memory_experiment(distance=3, rounds=1)
+    d2 = surface_code_memory_experiment(distance=3, rounds=2)
+
+    assert is_semantic_equivalence(d1, d2)
