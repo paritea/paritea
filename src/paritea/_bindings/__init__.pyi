8e7cf27af9c493424774d2190bfb121ee7e811d9
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2026-06-16 22:06:47 +0100

    refactor: Check fault equivalence in Rust and optimise (#35)

--- src/paritea/_bindings/__init__.pyi
+++ src/paritea/_bindings/__init__.pyi
@@ -7,3 +7,12 @@ def _compute_pauli_webs(
 def _compute_pauli_webs_through_partitions(
     a: Diagram, *, partitions: list[list[int]]
 ) -> tuple[list[PauliString], list[PauliString]]: ...
+def _check_fault_equivalence(
+    *,
+    nm1_sigs: list[tuple[int, int]],
+    nm2_sigs: list[tuple[int, int]],
+    d1_detectors: int,
+    d2_detectors: int,
+    until: int | None,
+    quiet: bool,
+) -> int | None: ...
